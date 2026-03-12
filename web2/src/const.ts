3308fa1e7208a0f5b3e80086eb4d533dedbfa38a
yukio-kayaba <zviamontevilca@gmail.com>
2026-03-12 02:16:22 -0500

    dwdw

--- web2/src/const.ts
+++ web2/src/const.ts
@@ -1,4 +1,4 @@
-export const RUTA ="https://https://api-yukio.ksdemosapps.com";
+export const RUTA ="https://api-yukio.ksdemosapps.com";
 export const dataPruebaz = "dataPruebaT";
 export const APPToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub21icmUiOiJlbmZlcm1lcmlhRGF0YSIsImlkUHJveWVjdCI6MSwiaWF0IjoxNzczMjkyMDIwfQ.h7rJMmRRQG4_A1raWMoytCuDL2GfKfDobVlIs1iqrTY";
