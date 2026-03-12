1e054b755dda569916d76515bde9a2087212ff5c
yukio-kayaba <zviamontevilca@gmail.com>
2026-03-12 11:39:13 -0500

    habilitando cors

--- app/app/src/main/java/com/example/hospital/core/network/ApiConfig.kt
+++ app/app/src/main/java/com/example/hospital/core/network/ApiConfig.kt
@@ -1,8 +1,8 @@
 package com.example.hospital.core.network
 
 object ApiConfig {
-    const val BASE_URL = "http://10.0.2.2:4000/"
-    const val TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub21icmUiOiJlbmZlcm1lcmlhRGF0YSIsImlkUHJveWVjdCI6MSwiaWF0IjoxNzczMjY3OTUwfQ.I0yI26-MScuKPQOpN1_ROow5nX65_JW9EHAjhoCqfrE"
+    const val BASE_URL = "https://server.zynova.online/"
+    const val TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub21icmUiOiJlbmZlcm1lcmlhRGF0YSIsImlkUHJveWVjdCI6MSwiaWF0IjoxNzczMjkyMDIwfQ.h7rJMmRRQG4_A1raWMoytCuDL2GfKfDobVlIs1iqrTY"
     const val BASIC_TOKEN = "z "+TOKEN
     
     fun buildImageUrl(path: String): String {
