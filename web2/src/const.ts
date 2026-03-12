89ecdf5a74c2cc0f10021ac4f2ed2b2b00f483ab
yukio-kayaba <zviamontevilca@gmail.com>
2026-03-12 02:02:23 -0500

    token BD

--- web2/src/const.ts
+++ web2/src/const.ts
@@ -1,7 +1,7 @@
 export const RUTA = "https://centro-medico.ksdemosapps.com";
 export const dataPruebaz = "dataPruebaT";
 export const APPToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub21icmUiOiJlbmZlcm1lcmlhRGF0YSIsImlkUHJveWVjdCI6MSwiaWF0IjoxNzczMjY3OTUwfQ.I0yI26-MScuKPQOpN1_ROow5nX65_JW9EHAjhoCqfrE";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub21icmUiOiJlbmZlcm1lcmlhRGF0YSIsImlkUHJveWVjdCI6MSwiaWF0IjoxNzczMjkyMDIwfQ.h7rJMmRRQG4_A1raWMoytCuDL2GfKfDobVlIs1iqrTY";
 
 export const dataCM = "dataCM"
 export const RESERVAS_STORAGE_KEY = "reservas_medicas";
