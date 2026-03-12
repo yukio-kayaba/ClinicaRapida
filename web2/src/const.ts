9fe00a10c31090e560599770aff526f5108c4103
yukio-kayaba <zviamontevilca@gmail.com>
2026-03-12 11:40:16 -0500

    cambiadno ruta a mi ruta

--- web2/src/const.ts
+++ web2/src/const.ts
@@ -1,4 +1,4 @@
-export const RUTA ="https://api-yukio.ksdemosapps.com";
+export const RUTA = "https://server.zynova.online";
 export const dataPruebaz = "dataPruebaT";
 export const APPToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub21icmUiOiJlbmZlcm1lcmlhRGF0YSIsImlkUHJveWVjdCI6MSwiaWF0IjoxNzczMjkyMDIwfQ.h7rJMmRRQG4_A1raWMoytCuDL2GfKfDobVlIs1iqrTY";
